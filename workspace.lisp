cb7f3bfb031d77a6d604446461417dfec82ac489
Patrick Logan <patrickdlogan@gmail.com>
2011-04-23 21:36:10 -0700

    Added some webdriver helpers and an initial lisp-unit test.

--- workspace.lisp
+++ workspace.lisp
@@ -11,6 +11,9 @@
 ;; simplified BSD license" See
 ;; http://www.opensource.org/licenses/bsd-license
 
+(ql:quickload "lisp-unit")
+(use-package 'lisp-unit)
+
 (defun make-html-unit-driver ()
   "Return a new instance of org.openqa.selenium.HtmlUnitDriver"
   (jnew "org.openqa.selenium.htmlunit.HtmlUnitDriver"))
@@ -19,3 +22,18 @@
   "Use the given driver to HTTP GET the url. The result is loaded into the driver's browser."
   (jcall "get" driver url))
 
+(defun find-element-by-tag (driver tag)
+  "Return an element with the given HTML tag name on the driver's current page."
+  (jcall "findElement" driver (jstatic "tagName" "org.openqa.selenium.By" tag)))
+
+(defun get-text (elem)
+  "Return the inner text of the given element."
+  (jcall "getText" elem))
+
+(define-test test-my-max
+  (assert-equal "Moo Foo" (let* ((driver (make-html-unit-driver))
+				 (elem   (progn (load-page driver "http://localhost:8000")
+						(find-element-by-tag driver "h1"))))
+			    (get-text elem))))
+
+;(run-tests)
